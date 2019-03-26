615210836b47930a17d8d64b7af293865e1c1054
Sabine Strasburger <sstrasburger451@gmail.com>
2019-03-26 13:30:13 -1000

    Added the login page & functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup', views.signup_view, name="signup"),
     path('userhome', views.user_homepage, name="userhome"),
+    path('login', views.login_view, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup_view(request):
@@ -11,7 +11,17 @@ def signup_view(request):
             return redirect('accounts:userhome')
     else:
         form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form':form})
+    return render(request, 'accounts/signup.html', { 'form': form })
 
 def user_homepage(request):
     return render(request, 'accounts/userhome.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            return redirect('accounts:userhome')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', { 'form': form })
