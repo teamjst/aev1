b5dc1574323bcc4984da41b50c6fa87c9f7d5537
Tysen Imai-Toyama <42785023+TysenImaiToyama@users.noreply.github.com>
2019-03-27 02:58:39 -1000

    User model made

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    fname = models.CharField(max_length=25)
+    lname = models.CharField(max_length=25)
+    uname = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+    email = models.CharField(max_length=25)
+    admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.uname
