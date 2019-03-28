4331f0db99de4aa79f93f7eb22da54b45a5ac851
Tysen Imai-Toyama <42785023+TysenImaiToyama@users.noreply.github.com>
2019-03-28 06:19:38 -1000

    Ip checking,
    Checks if IP entered matches the discovered IPS

--- adddevice/views.py
+++ adddevice/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render
-from yeelight import Bulb
+from yeelight import Bulb, discover_bulbs
 from .models import Settings
+import threading
+import queue
 
 # Create your views here.
 
 
 def adddevice_view(request):
-    #Sets up the IP list
+    # Multi-threaded so it doesnt take forever to load page
+    bulbqueue = queue.Queue()
+
+    def queuewrap(queue):
+        queue.put(discover_bulbs())
+    discoverthread = threading.Thread(target=queuewrap, args=(bulbqueue,))
+    discoverthread.start()
+    # Sets up the IP list
     queryset = Settings.objects.all()
     context = {
         "bulb_list": queryset
     }
+    discoverthread.join()
+    bulbs = bulbqueue.get()
     ip = request.GET.get('ip')
-    print(ip)
     if request.GET.get('connect'):
         bulb = Settings()
         bulb.ip = ip
         if not Settings.objects.filter(ip__contains=ip):
-            bulb.save()
+            for x in bulbs:
+                if x['ip'] == ip:
+                    bulb.save()
         print(Settings.objects.all())
     return render(request, 'adddevice/adddevice.html', context)
