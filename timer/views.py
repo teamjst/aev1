957170523e93b2908c004c587aaa1b042c811a4d
Tysen Imai-Toyama <42785023+TysenImaiToyama@users.noreply.github.com>
2019-03-29 14:26:36 -1000

    Timer page working

--- timer/views.py
+++ timer/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from adddevice.models import Settings
 from yeelight import Bulb
+import datetime
+import threading
+
 
 # Create your views here.
 def timer_view(request):
-    print(request.GET.get('timer'))
+    userbulb = Settings.objects.filter(username__exact=request.user)
+    startpause = False
+    bulb = Bulb(userbulb[0].ip)
+    if request.GET.get('timerstart'):
+        timeone = request.GET.get('timepicker-one')
+        timetwo = request.GET.get('timepicker-two')
+
+        def delZero(time):
+            intver = int(time[0])
+            if intver < 10:
+                return "0" + time
+            else:
+                return time
+        timeone = delZero(timeone)
+        timetwo = delZero(timetwo)
+        while timetwo != datetime.datetime.now().strftime("%I : %M %p"):
+            if timeone == datetime.datetime.now().strftime("%I : %M %p") and startpause is False:
+                bulb.turn_on()
+                startpause = True
+        bulb.turn_off()
     return render(request, 'timer/timer.html')
+
